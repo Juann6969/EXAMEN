8e7a97de712f11f0e7fdcb875607ca9f85fc5de3
Juan Manuel García Díaz-mariblanca <juanmanuel.s329162@cesurformacion.com>
2023-03-22 17:02:37 +0100

    RamaA2

--- src/sup1/Principal.java
+++ src/sup1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola a tod@s.");
+		System.out.println("Hola a tod@s.");  
 	}
 
 }
